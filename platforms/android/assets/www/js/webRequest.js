65d5b06f218df927fedb93c47c8bee62eaa4cb86
mobilesupport <cheehaung@Chees-MacBook-Air.local>
2016-02-15 17:29:34 +0800

    cm

--- platforms/android/assets/www/js/webRequest.js
+++ platforms/android/assets/www/js/webRequest.js
@@ -574,6 +574,7 @@ function postLocationUpdate(registrationId,latitude,longitude){
 }
 
 function getProfile(userId){
+    alert("getprogile");
     $.ajax({
       url: "http://192.168.1.18/MRWebApi/api/profile/statistic?userid=",
       type: "GET",
